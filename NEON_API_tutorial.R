57aa072c649c7c92ace5cb3dcf205b5c6761b7e0
Jack Hastings <33032523+Jack-Hastings@users.noreply.github.com>
2021-02-24 15:41:14 -0500

    intial commit

---
+++ NEON_API_tutorial.R
@@ -0,0 +1,14 @@
+#####################################
+## What: Jack learns how to use the NEON API: Data Download tutorial
+## Who: Jack Hastings
+## When: 2/24/2022
+## https://www.neonscience.org/resources/learning-hub/tutorials/neon-api-usage
+#####################################
+
+library(tidyverse)
+library(httr)
+library(jsonlite)
+library(devtools)
+library(downloader)
+library(geoNEON)
+library(neonUtilities)
